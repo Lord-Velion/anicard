7e5f173154ffc889aad9b85d335d1f4ecc14bf9d
Lord-Velion <61228826+Lord-Velion@users.noreply.github.com>
2026-05-08 14:08:03 +0500

    Add Character entity and configure EF Core mapping

--- Migrations/20260508090537_AddCharacterTable.Designer.cs
+++ Migrations/20260508090537_AddCharacterTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AniCard.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AniCard.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260508090537_AddCharacterTable")]
+    partial class AddCharacterTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.4");
